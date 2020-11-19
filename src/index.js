87dbaca052cc79590ad3258d705f161c99f3ee40
NehaKhann <n.nehakhan333@gmail.com>
2020-11-19 15:09:16 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//require react and react-dom
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+
+import React from "react";
+import ReactDOM from "react-dom";
+//render function
+// ReactDOM.render(WHAT TO SHOW,WHERE TO SHOW IT);
+ReactDOM.render(<h1>Neha Khan, a  programmer!</h1>, document.getElementById("root"));
+//pure js it is and very long
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hellow World!";
+// document.getElementById("root").appendChild(h1);
